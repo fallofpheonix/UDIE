45f54e3175d58fc20918b5bf5487f8cccacd2326
Ujjwal Singh <ujjosing@gmail.com>
2026-02-12 11:47:54 +0530

    Initial Commit

---
+++ UDIE/UDIEApp.swift
@@ -0,0 +1,32 @@
+//
+//  UDIEApp.swift
+//  UDIE
+//
+//  Created by Ujjwal Singh on 12/02/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct UDIEApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ UDIETests/UDIETests.swift
@@ -0,0 +1,17 @@
+//
+//  UDIETests.swift
+//  UDIETests
+//
+//  Created by Ujjwal Singh on 12/02/26.
+//
+
+import Testing
+@testable import UDIE
+
+struct UDIETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
